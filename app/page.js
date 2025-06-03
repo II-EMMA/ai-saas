c4597e364a73a902c049cdeb92f7970df3c95673
II-EMMA <emma.ahmedera@gmail.com>
2025-06-03 13:15:35 +0300

    app file save changes

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl btn-ghost btn-accent">
-        Accent
+        Accent Btn
       </button>
     </main>
   );
